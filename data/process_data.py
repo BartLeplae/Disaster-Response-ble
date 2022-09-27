c46251b152a6b82a77dc049d30180a1ef6acc828
Bart Leplae <bartleplae@hotmail.com>
2022-09-27 22:09:44 +0200

    corrected etl to integers, model with most fields

--- data/process_data.py
+++ data/process_data.py
@@ -63,8 +63,8 @@ def clean_data(df):
     for column in categories:
         categories[column] = categories[column].str[-1]
     
-    # convert column from string to numeric
-    categories[column] = categories[column].astype(int)
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
     
     # drop original column "categories" as now split
     df.drop(columns="categories", inplace=True)
