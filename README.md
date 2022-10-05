e5c807facf514e94f4b4be8237503105e403a866
Bart Leplae <bartleplae@hotmail.com>
2022-10-05 10:44:52 +0200

    Final cleanup

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Disaster-Response-ble
-Analyze disaster data from Appen
-Provide a model that classifies master disaster messages for use by multiple parties involved with the Disaster
+Analyze disaster data provided by Appen
+Provide a model that classifies master disaster messages for use by multiple parties involved with a Disaster
 
 ## Installation
 Python:
-...
+        sys
+        json
+        string
+        plotly
+        pandas
+        numpy
+        joblib
+        stats
+        nltk
+        flask
+        plotly.graph_objs plotly.express
+        sqlalchemy sqlite3
+        sklearn
+        pickle
 
 
 ## Project motivation
@@ -22,21 +35,26 @@ This approach will have the benefit of expediting the distrubtion of relevant in
 
 ## File Descriptions
 Code:
-- IncidentTicketAnalysis.ipynb (Jupyter notebook / Python code)
+- load, clean data and store in db: data\ETL Pipeline Preparation.ipynb, data\process_data.py 
+- create ML model: models\ML Pipeline Preparation.ipynb, model\train_classifier.py
+- show data locally: app\run.py (using templates: app\templates)
+- show data in Heroku: app.py (using templates: .\templates)
 
 Input:
 - disaster_messages.csv: Messages
-- disaster_categories.csv: Categories (determined manually as input to the supervised model)
+- disaster_categories.csv: Categories (determined manually, serves as input to the supervised model)
 
 Output:
 - DisasterResponse.db: SQLight database with cleaned message and categorization data
 - Results of test sets
 - classifier.pkl: Final model as a pickle file (e.g.: for use by Web page that can auto-categorize)
+- website https://disaster-response-ble.herokuapp.com/ (Excludes the "Classify Message" functionality due to Heroku limitations)
 
 ## Technical details
 - Step 1: Run ETL pipeline that processes the messages and their corresponding categories and store these in a SQLight database
 - Step 2: Run Supervised ML model with Multi-output
-- Step 3: Display relevant statistics through Web Application (Flask)
+- Step 3: Display relevant statistics + interactive use of ML classifier through local Web Application (Flask)
+- Step 4: Display relevant statistics through Web Application (Flask) hosted on Heroku server
 
 ## Instructions:
 1. To run ETL pipeline that cleans data and stores in database.
@@ -46,9 +64,19 @@ Output:
 3. To run web app: 
         `cd app`
         `python run.py`
-        Click the `PREVIEW` button to open the homepage
+        open homepage on http://127.0.0.1:3000
+4. To depict in Heroku: 
+        Deployed by connecting through GitHub 
+        Procfile contains reference to app.py which is at the top level folder:  "web: gunicorn app:app"
+        result in https://disaster-response-ble.herokuapp.com/
+
+## Remarks
+The first chart is a correlation matrix which depicts the correspondence between categories ('child_alone' shows a white vertical and horizontal bar)
+The last chart shows the number of occurences in the training set ordered by volume.
+1. The child_alone category has no occurences (has been excluded from the ML model to avoid modelling failures)
+2. The categories offer, shops, tools, hospitals and missing_people have less than 300 occurences which limits the robustness of the ML model
+
 
 ## Licensing, Authors, Acknowledgements
 - Author:Bart Leplae
 - Acknowledgement: Made use of Udacity course materials
-
---
+++ app/run.py
@@ -0,0 +1,178 @@
+""" run:
+
+    Creates webpage with:
+    - functionality to classify messages
+    - Overview of training dataset by means of various plots 
+    
+    to run: python run.py
+
+Attributes:
+    None
+
+Input:
+    Training Data with messages and classification: 
+        database: "../data/DisasterResponse.db": 
+        table: "DisasterMessages"
+    ML model to classify messages into different categories: ../models/DisasterResponse.pkl
+    
+Output:
+    Webpage: http://127.0.0.1:3000/
+"""
+
+#%%
+import json
+import plotly
+import pandas as pd
+import joblib
+import string
+import nltk
+# from scipy import stats - Remove as not running on Heroku
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+import plotly.express as px
+from sqlalchemy import create_engine
+#%%
+
+# Initiate Flask application
+app = Flask(__name__)
+app.secret_key = "whatever_blabla"
+
+# Function to tokenize the messages entered in the Webpage (input to the ML model)
+def tokenize(text):
+    # Bring to lowercase, remove stopwords and punctuation
+    stop = set(stopwords.words('english') + list(string.punctuation))
+    nltk_tokens = nltk.word_tokenize(text.lower())
+    tokens = [w for w in nltk_tokens if not w in stop]
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(w) for w in tokens]
+    return(tokens)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load message training data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterMessages', engine)
+df["message_length"] = df["message"].str.len()
+
+# load trained model
+model = joblib.load("../models/DisasterResponse.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # Create 6 graphs
+    graphs = [None]*7
+
+    # Graph 1: Correlation matrix to depict relationship between message categories
+    correlation_matrix = df.corr()
+    graphs[0] = px.imshow(correlation_matrix)
+
+    # Graph 2: Bargraph to depict the categories that go along with the "aid_related" category
+    # The correlation matrix depicts that there are several correlations
+    df_line = df[df["aid_related"]==1]
+    df_line = df_line.drop(columns=["id","message","genre","aid_related","message_length"])
+    df_line = df_line.sum()
+    df_line = df_line.reset_index()
+    df_line.columns=(["Category","Count"])
+    df_line = df_line.sort_values(by="Count",ascending=False)
+    graphs[1] = px.bar(df_line,x="Category",y="Count")
+
+    # Graph 3: Bargraph depicting the number of messages per 'genre'
+    df_genre_counts = df.groupby('genre', as_index=False).agg(count=('message','count'))   
+    graphs[2] = px.bar(df_genre_counts, x="genre", y="count", barmode="stack",title="Aid Related correlation")
+
+    # Graph 4: Bargraph depicting the number of messages per 'genre', differentiated by "Aid related"
+    df_aid_related = df.groupby(['aid_related','genre'], as_index=False).agg(count=('message','count'))
+    df_aid_related["aid"] = df_aid_related["aid_related"].map({0: "Not Aid Related", 1: "Aid Related",})
+    graphs[3] = px.bar(df_aid_related, x="genre", y="count", color="aid", barmode="stack", title="#Aid related")
+
+    # Graph 5: Bargraph depicting the ratio of "Aid related" messages per 'genre'
+    df_aid_related["count2"] = df_aid_related["count"].div(df_aid_related.groupby("genre")["count"].transform("sum"))
+    graphs[4] = px.bar(df_aid_related, x="genre", y="count2", color="aid", barmode="stack", title="%Aid related")
+    
+    # Graph 6: Boxplot depicting the length of the messages per "genre"
+    df_message_len = df[["genre","message_length"]]
+    graphs[5] = px.box(df_message_len, x="genre", y="message_length", log_y=True, title="message length")
+
+    # Graph 7: Bargraph to depict the number of category occurences in the database
+    df_categories = df
+    df_categories = df_categories.drop(columns=["id","message","genre","message_length"])
+    df_categories = df_categories.sum()
+    df_categories = df_categories.reset_index()
+    df_categories.columns=(["Category","Count"])
+    df_categories = df_categories.sort_values(by="Count",ascending=False)
+    graphs[6] = px.bar(df_categories,x="Category",y="Count",title="Number of category occurences in training set")
+
+    # encode plotly graphs in JSON
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+    # Transform the message length
+    # mess_length = stats.percentileofscore(df["message_length"],len(query))  Remove as not running on Heroku
+    mess_length = 0.2 # Replace with constant (short sentence)
+    # Check for availability of Question and Exclamation mark
+    if '\?' in query:
+        question_mark = 1
+    else:
+        question_mark = 0
+
+    if '\!' in query:
+        exclamation_mark = 1
+    else:
+        exclamation_mark = 0 
+
+    # Create dataframe to serve as input to the ML model
+    X_query = pd.DataFrame(data = [[query,"direct",mess_length,question_mark,exclamation_mark]], columns=["message","genre","len","question_mark","exclamation_mark"])
+
+    # use model to predict classification for query
+    classification_labels = model.predict(X_query)[0]
+    # classification_labels = model.predict(query)
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
+# %%
