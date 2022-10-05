2df9ef90d491d72e72a6a3b9071497c701bab874
Bart Leplae <bartleplae@hotmail.com>
2022-10-05 07:56:13 +0200

    6 graphs

--- app.py
+++ app.py
@@ -20,13 +20,38 @@ df["message_length"] = df["message"].str.len()
 def index():
 
     # Create 6 graphs
-    graphs = [None]*1
+    graphs = [None]*6
 
     # Graph 1: Correlation matrix to depict relationship between message categories
     correlation_matrix = df.corr()
     graphs[0] = px.imshow(correlation_matrix)
 
-    # figures = return_figures()
+    # Graph 2: Bargraph to depict the categories that go along with the "aid_related" category
+    # The correlation matrix depicts that there are several correlations
+    df_line = df[df["aid_related"]==1]
+    df_line = df_line.drop(columns=["id","message","genre","aid_related","message_length"])
+    df_line = df_line.sum()
+    df_line = df_line.reset_index()
+    df_line.columns=(["Category","Count"])
+    df_line = df_line.sort_values(by="Count",ascending=False)
+    graphs[1] = px.bar(df_line,x="Category",y="Count")
+
+    # Graph 3: Bargraph depicting the number of messages per 'genre'
+    df_genre_counts = df.groupby('genre', as_index=False).agg(count=('message','count'))   
+    graphs[2] = px.bar(df_genre_counts, x="genre", y="count", barmode="stack",title="Aid Related correlation")
+
+    # Graph 4: Bargraph depicting the number of messages per 'genre', differentiated by "Aid related"
+    df_aid_related = df.groupby(['aid_related','genre'], as_index=False).agg(count=('message','count'))
+    df_aid_related["aid"] = df_aid_related["aid_related"].map({0: "Not Aid Related", 1: "Aid Related",})
+    graphs[3] = px.bar(df_aid_related, x="genre", y="count", color="aid", barmode="stack", title="#Aid related")
+
+    # Graph 5: Bargraph depicting the ratio of "Aid related" messages per 'genre'
+    df_aid_related["count2"] = df_aid_related["count"].div(df_aid_related.groupby("genre")["count"].transform("sum"))
+    graphs[4] = px.bar(df_aid_related, x="genre", y="count2", color="aid", barmode="stack", title="%Aid related")
+    
+    # Graph 6: Boxplot depicting the length of the messages per "genre"
+    df_message_len = df[["genre","message_length"]]
+    graphs[5] = px.box(df_message_len, x="genre", y="message_length", log_y=True, title="message length")
 
     # plot ids for the html id tag
     ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)] 
